4474c1b896fbb33d37b06be3d831866d6df88642
yasanagui <yasanagui8@gmail.com>
2015-08-19 12:55:55 -0500

    Ver si se ve la carpeta source

---
+++ prueba/src/java/JavaBean/BeanGraficaPersonal.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JavaBean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author artur
+ */
+@ManagedBean
+@RequestScoped
+public class BeanGraficaPersonal {
+
+    /**
+     * Creates a new instance of BeanGraficaPersonal
+     */
+    public BeanGraficaPersonal() {
+    }
+    
+}
